7770f3a471bce5949b82b8e56b8a57fc0340ca1d
AleFolla2000 <alessandro.folla@studenti.unimi.it>
2021-06-09 12:24:05 +0200

    MappaConPosizione
    Funziona anche la mappa con la posizione

--- public/javascripts/meteoc.js
+++ public/javascripts/meteoc.js
@@ -18,13 +18,33 @@ function chiamataServizio()
             var latitudine = responseJson.location.lat;
             console.log(latitudine);
             
-            var Longitudine = responseJson.location.lat;
+            var Longitudine = responseJson.location.lon;
             console.log(Longitudine);
 
-            //console.log(responseJson.location.region);
-            //newDiv.innerHTML=responseJson.location.lon;
-            //document.body.appendChild(newDiv);
+            initMap(latitudine,Longitudine);
 
+            //Ultimo Aggiornamento
+            var update = document.createElement("text");
+            update.innerHTML="Ultimo aggiornamento: "+responseJson.current.last_updated;
+            document.body.appendChild(update);
+            //Temperatura
+            var temperatura = document.createElement("text");
+            temperatura.innerHTML="<br>Temperatura C: "+responseJson.current.temp_c + "<br>Temperatura F: "+responseJson.current.temp_f;
+            document.body.appendChild(temperatura);
+            //Condizioni
+            var condizioni = document.createElement("text");
+            condizioni.innerHTML="<br>Condizioni meteo: "+responseJson.current.condition.text;
+            document.body.appendChild(condizioni);
+            //Immagine
+            var icona = document.createElement("img");
+            icona.innerHTML=responseJson.current.condition.icon;
+            document.body.appendChild(icona);
+            /*
+           //vento 
+           var vento = document.createElement("text");
+           vento.innerHTML="<br>Temperatura C: "+responseJson.current.temp_c + "<br>Temperatura F: "+responseJson.current.temp_f;
+           document.body.appendChild(Vento);
+                */
             }else{
                 console.log("ERRORE");
             }
@@ -43,18 +63,21 @@ function chiamataServizio()
 
 }
 
-function initMap(){
-    //opzioni 
-    var options = {
-        zoom:10,
-        center: {lat: 46.1324186, lng:9.5694737}
+function initMap(x,y){
+    if(x !=undefined && y!=undefined)
+    {
+      //opzioni 
+      var options = {
+           zoom:10,
+           center: {lat: x, lng:y}
+        }
+        //creazione della mappa
+        var map = new google.maps.Map(document.getElementById('map'),options);
+     //aggiungo il marker
+     var marker = new google.maps.Marker({
+            position: {lat: x, lng:y},
+            map:map
+        })  
     }
-    //creazione della mappa
-    var map = new google.maps.Map(document.getElementById('map'),options);
-    //aggiungo il marker
-    var marker = new google.maps.Marker({
-        position: {lat: 46.1324186, lng:9.5694737},
-        map:map
-    })
 
 }

5a4e5324f2f7203a9c0b321c75b18c96d2ca4eaf
alefolla <folla.alessandro@gmail.com>
2021-06-13 18:11:03 +0200

    END

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Easy-Weather' });
+  res.render('index', { title: 'EasyTopWeather' });
 });
 
 module.exports = router;
